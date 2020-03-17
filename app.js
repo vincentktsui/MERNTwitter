4db21e601f6d9dc012ccb8b45a6f03b67e54525b
vincentktsui <vincentktsui@gmail.com>
2020-03-16 18:10:11 -0700

    Put back listen port

--- app.js
+++ app.js
@@ -29,3 +29,6 @@ require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
